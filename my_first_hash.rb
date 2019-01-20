c05bbdafa4ab538a2f4d7372a86ad1aaf3bf57c8
Emmanuel Anzele <eanzele@gmail.com>
2019-01-20 21:48:22 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,4 @@
-def my_hash
+def my_hash = {"Pancakes" => "awesome", "Pharmacy" => "Motrin", "Hospital" => "Grady"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
